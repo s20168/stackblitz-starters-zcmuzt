c6744cd457addfedad2bef480e696098234eac15
s20168 <56789654+s20168@users.noreply.github.com>
2023-06-24 06:45:12 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# stackblitz-starters-zcmuzt
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/stackblitz-starters-zcmuzt)
---
+++ src/App.tsx
@@ -0,0 +1,73 @@
+import * as React from 'react'
+import PropTypes from 'prop-types'
+import './style.css'
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Link,
+  useLocation,
+} from 'react-router-dom'
+
+const Home: React.FC = () => <h1>Strona główna</h1>
+const About: React.FC = () => <h1>O nas</h1>
+const Services: React.FC = () => <h1>Usługi</h1>
+const Contact: React.FC<{ countryCode?: 'pl' | 'us' | 'de' }> = ({
+  countryCode,
+}) => {
+  return <h1>Kontakt {countryCode}</h1>
+}
+
+Contact.propTypes = {
+  countryCode: PropTypes.oneOf(['pl', 'us', 'de']),
+}
+
+const NotFound: React.FC = () => {
+  const location = useLocation()
+  return <h1>Nie znaleziono elementu: {location.pathname}</h1>
+}
+
+const App = () => (
+  <Router>
+    <div>
+      <nav>
+        <ul className="navbar">
+          <li>
+            <Link to="/">Strona główna</Link>
+          </li>
+          <li>
+            <Link to="/about">O nas</Link>
+          </li>
+          <li>
+            <Link to="/services">Usługi</Link>
+          </li>
+          <li>
+            <Link to="/contact">Kontakt</Link>
+          </li>
+          <li>
+            <Link to="/contact/us">Kontakt (US)</Link>
+          </li>
+          <li>
+            <Link to="/contact/pl">Kontakt (PL)</Link>
+          </li>
+          <li>
+            <Link to="/contact/de">Kontakt (DE)</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route index Component={Home} />
+        <Route path="/about" Component={About} />
+        <Route path="/services" Component={Services} />
+        <Route path="/contact" Component={Contact} />
+        <Route path="/contact/pl" element={<Contact countryCode="pl" />} />
+        <Route path="/contact/de" element={<Contact countryCode="de" />} />
+        <Route path="/contact/us" element={<Contact countryCode="us" />} />
+        <Route path="*" Component={NotFound} />
+      </Routes>
+    </div>
+  </Router>
+)
+
+export default App
