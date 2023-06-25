341ff092697b75b5abe55e17361e2857e6040d1a
s20168 <56789654+s20168@users.noreply.github.com>
2023-06-25 15:31:42 +0200

    2 files modified, 2 added

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,30 @@
-import * as React from 'react'
-import PropTypes from 'prop-types'
-import './style.css'
+import React from 'react';
+import PropTypes from 'prop-types';
+import './style.css';
 import {
   BrowserRouter as Router,
   Route,
   Routes,
   Link,
   useLocation,
-} from 'react-router-dom'
+} from 'react-router-dom';
 
-const Home: React.FC = () => <h1>Strona główna</h1>
-const About: React.FC = () => <h1>O nas</h1>
-const Services: React.FC = () => <h1>Usługi</h1>
-const Contact: React.FC<{ countryCode?: 'pl' | 'us' | 'de' }> = ({
-  countryCode,
-}) => {
-  return <h1>Kontakt {countryCode}</h1>
-}
+const Home = () => <h1>Strona główna</h1>;
+const About = () => <h1>O nas</h1>;
+const Services = () => <h1>Usługi</h1>;
+
+const Contact = ({ countryCode }) => {
+  return <h1>Kontakt {countryCode}</h1>;
+};
 
 Contact.propTypes = {
   countryCode: PropTypes.oneOf(['pl', 'us', 'de']),
-}
+};
 
-const NotFound: React.FC = () => {
-  const location = useLocation()
-  return <h1>Nie znaleziono elementu: {location.pathname}</h1>
-}
+const NotFound = () => {
+  const location = useLocation();
+  return <h1>Nie znaleziono elementu: {location.pathname}</h1>;
+};
 
 const App = () => (
   <Router>
@@ -57,17 +56,17 @@ const App = () => (
       </nav>
 
       <Routes>
-        <Route index Component={Home} />
-        <Route path="/about" Component={About} />
-        <Route path="/services" Component={Services} />
-        <Route path="/contact" Component={Contact} />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact countryCode={null} />} />
         <Route path="/contact/pl" element={<Contact countryCode="pl" />} />
         <Route path="/contact/de" element={<Contact countryCode="de" />} />
         <Route path="/contact/us" element={<Contact countryCode="us" />} />
-        <Route path="*" Component={NotFound} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
   </Router>
-)
+);
 
-export default App
+export default App;
